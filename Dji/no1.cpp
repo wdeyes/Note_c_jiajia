f99bd7af1bcfcfba4e8121ae06a6f72324921e63
wdeye <wd_wangdong@foxmail.com>
2019-08-11 22:05:43 +0800

    modify Dji

--- Dji/no1.cpp
+++ Dji/no1.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 using namespace std;
 
+// 递归，组合
 void solve(vector<pair<int, int> > game, int day_num, int index, int &ans, int &max){
 	if(index==game.size() && day_num>=0){
 		if(ans>max)
@@ -12,15 +13,34 @@ void solve(vector<pair<int, int> > game, int day_num, int index, int &ans, int &
 	if(index==game.size() && day_num<0)
 		return ;
 	
-	// day_num -= game[index].second;
-	// ai += game[index].first;
+
 	ans += game[index].first;
 	solve(game, day_num-game[index].second, index+1, ans, max);
-	//day_num += game[index].second;
-	//ai -= game[index].first;
+
 	ans -= game[index].first;
 	solve(game, day_num, index+1, ans, max);
 
+}
+
+// 01背包问题，动态规划
+// https://blog.csdn.net/yimingsilence/article/details/82628757
+// 递推公式：f(i,v) =max( f(i-1,v), f(i-1,v-c_i)+w_i )，对于dii件物品，背包容量为v
+// 优化过空间，仅使用一维数组。主要要倒着更新 j。j表示容量为j时。
+// 题外话：01背包有两种：背包够放和背包恰好放满，初始化不同。对于恰好用完的情况：
+// 					对于第0件，容量为0时初始为0，其余为负无穷，这样f(i,v)除非你用完v,不然+w_i也是无穷。
+// 						负无穷写法：#include<limits.h> INT_MIN 表示负无穷。
+//       完全背包问题：更新方向不同，对于第i件可以要0~k个。容量要顺序更新，v是逐渐增大的，
+//					这样每次求最大时就包含了这件物品要g(0<g<k)件的可能。
+int solve2(vector<pair<int,int> > game, int day_num, int game_num){
+	vector<int> dp(day_num+1);
+	for(int i=0; i<game_num; ++i){
+		int cost = game[i].second;
+		int value = game[i].first;
+		for(int j=day_num; j>=cost; --j){
+			dp[j]=max(dp[j], dp[j-cost]+value);
+		}
+	}
+	return dp[day_num];
 }
 
 int main(int argc, char const *argv[])
@@ -34,9 +54,10 @@ int main(int argc, char const *argv[])
 		for(int i=0; i<game_num; ++i){
 			cin >> game[i].first >> game[i].second;
 		}
-		int max=0, ans=0;
-		solve(game, day_num, 0, ans, max);
-		cout << max << endl;
+		// int max=0, ans=0;
+		// solve(game, day_num, 0, ans, max);
+		// cout << max << endl;
+		cout << solve2(game, day_num, game_num) << endl;
 	}
 	return 0;
 }
--- Dji/no2.cpp
+++ Dji/no2.cpp
@@ -11,13 +11,12 @@ int main(int argc, char const *argv[])
 	int n ,m;
 	while(cin >> n){
 		cin >> m;
-		//vector< pair<string, string> > p(n);
+
 		unordered_map<string, string> hash;
 		for(int i=0; i<n; ++i){
 			string cmd, action;
 			cin >> cmd >> action;
 			hash[cmd] = action;
-			//cin >> p[i].first >> p[i].second;
 		}
 		vector<string> p_cmd(m);
 		for(int i=0; i<m; ++i){
